c26ce9bbc4e6a859530e17c6e2db72fc8837950a
ftedone-linearbeam <fabio.tedone@linearbeam.com>
2025-07-10 16:41:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -174,24 +174,25 @@ if uploaded_file:
                     hovermode="x unified"
                 )
                 st.plotly_chart(fig1, use_container_width=True)
+                [Col1,Col2]=st.colums(2)
+                with Col1:
+                    # Assi X (tempo)
+                    x_min = st.number_input("Limite minimo asse X (secondi)", min_value=0, max_value=int(time.max()), value=int(filtered_time.min()))
+                    x_max = st.number_input("Limite massimo asse X (secondi)", min_value=0, max_value=int(time.max()), value=int(filtered_time.max()))
+                with Col2:
+                    # Assi Y (corrente, in μA o unità che usi)
+                    y_min = st.number_input("Limite minimo asse Y", value=float(filtered_data.min()))
+                    y_max = st.number_input("Limite massimo asse Y", value=float(filtered_data.max()))
+
+                fig1.update_xaxes(range=[x_min, x_max])
+                fig1.update_yaxes(range=[y_min, y_max])
+                
                 # Tabella filtrata
                 st.subheader("Dati Filtrati")
                 st.dataframe(pd.DataFrame({
                     "Tempo (s)": filtered_time,
                     "Dati normalizzati (-)": filtered_data
                 }))
-
-                # Assi X (tempo)
-                x_min = st.number_input("Limite minimo asse X (secondi)", min_value=0, max_value=int(time.max()), value=int(filtered_time.min()))
-                x_max = st.number_input("Limite massimo asse X (secondi)", min_value=0, max_value=int(time.max()), value=int(filtered_time.max()))
-                
-                # Assi Y (corrente, in μA o unità che usi)
-                y_min = st.number_input("Limite minimo asse Y", value=float(filtered_data.min()))
-                y_max = st.number_input("Limite massimo asse Y", value=float(filtered_data.max()))
-
-                fig1.update_xaxes(range=[x_min, x_max])
-                fig1.update_yaxes(range=[y_min, y_max])
-
                 st.subheader("Download")
                 
                 # ---- Esportazione in Excel ----
