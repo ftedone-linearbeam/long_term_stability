9f9286f70640083c069352748fff437a1533d1b8
ftedone-linearbeam <fabio.tedone@linearbeam.com>
2025-07-10 12:25:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Analisi Misurazioni Elettriche (mA)")
+st.title("Long term stability analysis (mA)")
 
 # Caricamento file Excel
 uploaded_file = st.file_uploader("Carica file Excel", type=["xlsx", "xls"])
